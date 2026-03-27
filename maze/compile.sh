cf81c6fc233259b006d16c1f899bf6cd1e79a359
SamShtram <sammshtramm@gmail.com>
2026-03-27 15:50:25 -0400

    Update shell script

--- maze/compile.sh
+++ maze/compile.sh
@@ -1,2 +1,2 @@
-gcc maze_sdl2.c astar.c -o maze_sdl2 `sdl2-config --cflags --libs` -lcurl -lhiredis $(pkg-config --cflags --libs libmongoc-1.0) -lm
+gcc -g -O0 -o maze_sdl2 maze_sdl2.c astar.c maze_learning.c $(sdl2-config --cflags --libs) $(pkg-config --cflags --libs libbson-1.0 libmongoc-1.0 hiredis libcurl) -lm
 ./maze_sdl2
